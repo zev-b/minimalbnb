49f50227ef7c6d3318ecf0daacdb1f49c48a6e1d
zev-b <zevbistritz@gmail.com>
2024-10-10 19:23:47 -0400

    Completed Phase 2, Note: the stylesheet for login is styling signup as well, why? Also, add styles and uncomment stylesheet import on signup component

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,7 +1,7 @@
 import { csrfFetch } from "./csrf";
 
-const LOGIN_USER = 'session/LOGIN_USER';
-const LOGOUT_USER = 'session/LOGOUT_USER';
+const LOGIN_USER = 'session/loginUser';
+const LOGOUT_USER = 'session/logoutUser';
 
 /**
  * A User object from the backend DB
@@ -11,6 +11,13 @@ const LOGOUT_USER = 'session/LOGOUT_USER';
  * @property { username } string
  * @property { firstName } string
  * @property { lastName } string
+ */
+
+/**
+ * 
+ * @typedef { Object } UserWithPassProperties 
+ * @property { string } password
+ * @typedef { User & UserWithPassProperties } UserWithPassword
  */
 
 export const loginUser = (user) => {
@@ -25,6 +32,7 @@ export const logoutUser = () => {
         type: LOGOUT_USER,
     }
 };
+
 
 /**
  * Send a request to the login endpoint on the backend
@@ -48,6 +56,13 @@ export const login = ({ credential, password }) => async (dispatch) => {
     }
 };
 
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(loginUser(data.user));
+    return response;
+};
+
 export const logout = () => async (dispatch) => {
     const res = await csrfFetch('/api/session', {
         method: "DELETE",
@@ -56,6 +71,28 @@ export const logout = () => async (dispatch) => {
         dispatch(logoutUser());
         return res.json();
     }
+};
+
+/**
+ * Send a request to the signup endpoint on the backend
+ * @param { UserWithPassword } user The username/email/... and password for the user
+ * @returns { User } The user object returned from the backend when signup
+ */
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password
+      })
+    });
+    const data = await response.json();
+    dispatch(loginUser(data.user));
+    return response;
 };
 
 const initialState = { user: null };
