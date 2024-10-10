c8db4838c9f9d8cb1c1e4d3f87037bb395556e47
zev-b <zevbistritz@gmail.com>
2024-10-09 21:58:05 -0400

    Completed Phase-0

---
+++ frontend/src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
+
+
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
