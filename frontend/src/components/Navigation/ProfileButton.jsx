4b57fa9dbee73ac8789739dbdde78bc389621dbe
zev-b <zevbistritz@gmail.com>
2024-10-10 19:55:05 -0400

    Added navigtion components, still in the works tho. (they said to commit)

---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,13 @@
+// import { PiUserSwitchThin } from "react-icons/pi"; 
+import { HiMiniUser } from "react-icons/hi2";
+
+const ProfileButton = () => {
+    return (
+        <div style={{ color: "black", fontSize: "40px" }}>
+            {/* <PiUserSwitchThin /> */}
+            <HiMiniUser />
+        </div>
+    )
+}
+
+export default ProfileButton;
