3a399a3cec95350b7df67224ad39e7f23e2f2ae2
zev-b <zevbistritz@gmail.com>
2024-10-10 17:20:17 -0400

    Refactored session.js added JS doc, added LoginForm component, and app route/page for navigation, added stylesheet for LoginForm

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,6 +3,15 @@ import { csrfFetch } from "./csrf";
 const LOGIN_USER = 'session/LOGIN_USER';
 const LOGOUT_USER = 'session/LOGOUT_USER';
 
+/**
+ * A User object from the backend DB
+ * @typedef { Object } User
+ * @property { number } id
+ * @property { string } email
+ * @property { username } string
+ * @property { firstName } string
+ * @property { lastName } string
+ */
 
 export const loginUser = (user) => {
     return {
@@ -11,29 +20,18 @@ export const loginUser = (user) => {
     };
 };
 
-// * do we pass in user?  (user)
 export const logoutUser = () => {
     return {
         type: LOGOUT_USER,
     }
-}
+};
 
-// *======== first draft login thunk action ==========
-// export const login = (user) => async (dispatch) => {
-//     const response = await csrfFetch('/api/session', {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             credential: user.credential,
-//             password: user.password
-//         })
-//     }); 
-//     const session = await response.json();
-//     dispatch(loginUser(session.user));
-//     return session;
-// }
-// *==================================================
-
-export const login = (credential, password) => async (dispatch) => {
+/**
+ * Send a request to the login endpoint on the backend
+ * @param {{ credential: string, password: string }} userLogin The username/email and password for the user
+ * @returns { User } The user object returned from the backend
+ */
+export const login = ({ credential, password }) => async (dispatch) => {
     const res = await csrfFetch('/api/session', {
         method: 'POST',
         body: JSON.stringify({
@@ -41,9 +39,14 @@ export const login = (credential, password) => async (dispatch) => {
             password,
         })
     });
-    dispatch(loginUser((await res.json()).user));
-    return res;
-}
+
+    if (res.ok) {
+        const user = (await res.json()).user;
+        dispatch(loginUser(user));
+    
+        return user;
+    }
+};
 
 export const logout = () => async (dispatch) => {
     const res = await csrfFetch('/api/session', {
@@ -53,8 +56,7 @@ export const logout = () => async (dispatch) => {
         dispatch(logoutUser());
         return res.json();
     }
-}
-
+};
 
 const initialState = { user: null };
 
