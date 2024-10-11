7a3790c69f1a4ab0240d98f9b252c72ec2929aea
zev-b <zevbistritz@gmail.com>
2024-10-10 21:22:42 -0400

    Completed pahse 3. 7th rendition of Navigation/ProfileButton component, just got the togglemenu finalized(they said to commit)

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,46 +1,38 @@
-// import { PiUserSwitchThin } from "react-icons/pi"; 
-import { HiMiniUser } from "react-icons/hi2";
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import * as sessionActions from '../../store/session';
-
-// const ProfileButton = () => {
-//     return (
-//         <div style={{ color: "black", fontSize: "40px" }}>
-//             {/* <PiUserSwitchThin /> */}
-//             <HiMiniUser />
-//         </div>
-//     )
-// }      
-{/* <HiMiniUser /> */}
-// 
-
-// export default ProfileButton;
-
 // frontend/src/components/Navigation/ProfileButton.jsx
+
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { HiMiniUser } from "react-icons/hi2";
+import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+    // if (!showMenu) setShowMenu(true);
+    setShowMenu(!showMenu);
   };
 
   useEffect(() => {
-    const closeMenu = () => {
-      setShowMenu(false);
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
     };
 
     document.addEventListener('click', closeMenu);
 
     return () => document.removeEventListener('click', closeMenu);
-  }, []);
+  }, [showMenu]);
 
-  const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-    setShowMenu(!showMenu);
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
@@ -50,7 +42,7 @@ function ProfileButton({ user }) {
       <button onClick={toggleMenu}>
       <HiMiniUser />
       </button>
-      <ul className={ulClassName}>
+      <ul className={ulClassName} ref={ulRef}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
