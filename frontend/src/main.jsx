1a24c1a39e99e71dbec241d586ecb6472b1f257a
zev-b <zevbistritz@gmail.com>
2024-10-10 22:16:10 -0400

    added a LoginFormModal. 9th edition of Navigation.jsx. OpenModalButton. 76.68% of Phase 4 readme complete. Up to Signup FormModal (they said to commit three times)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,9 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
-import * as sessionActions from './store/session';
-
-
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+import { ModalProvider, Modal } from './context/Modal';
 
 const store = configureStore();
 
@@ -21,9 +20,12 @@ if (import.meta.env.MODE !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+       <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
 
