efd562e14bf7b7ad602ce193caa6678d308aa216
zev-b <zevbistritz@gmail.com>
2024-10-22 17:58:22 -0400

    deploying 5:57pm 10/22

--- backend/db/migrations/20240922180522-create-spot.js
+++ backend/db/migrations/20240922180522-create-spot.js
@@ -59,7 +59,7 @@ module.exports = {
         allowNull: false,
       },
       description: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(1024),
         allowNull: false,
       },
       price: {
